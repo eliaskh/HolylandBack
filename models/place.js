c1e2bd3def76baa036a02060345094313bef1833
elias <eliaskhs@hotmail.com>
2022-12-14 01:14:36 +0200

    final corrections

--- models/place.js
+++ models/place.js
@@ -7,83 +7,85 @@ const placeSchema = new Schema({
   image: { type: String, required: false },
   category: { type: String, required: false },
   city: { type: String, required: false },
-  link: { type: String, required: false },
   tag: { type: String, required: false },
-  long: { type: String, required: false },
-  creator: { type: String, required: false },
-  threeSixtyImage: { type: String, required: false },
-  userId: { type: String, required: false },
-  time: { type: String, required: false },
-  passwordo: {
-    type: String,
-  },
-  havePassword: {
-    type: Boolean,
-  },
-  useit: {
-    type: Boolean,
-  },
-  publish: {
-    type: Boolean,
-  },
-  rotation: {
-    type: Boolean,
-  },
-  openDescription: {
-    type: Boolean,
-  },
-  loop: {
-    type: Boolean,
-  },
-  direction: {
-    type: String,
-  },
-  zoom: {
-    type: String,
-  },
-  zoom: {
-    type: String,
-  },
-  pause: {
-    type: Boolean,
-  },
-  showImageFeaturedInPause: {
-    type: Boolean,
-  },
-  disTourTitle: {
-    type: Boolean,
-  },
-  playicon: {
-    type: String,
-  },
-  pauseOpacity: {
-    type: String,
-  },
-  cssTourTitle: {
-    type: String,
-  },
-  EnableLine: {
-    type: Boolean,
-  },
-  LineTitle: {
-    type: String,
-  },
-  tourHd: {
-    type: String,
-  },
-  rotationSpeed: { type: String },
   imgsData: [],
   date: {
     type: Date,
     default: Date.now,
   },
-  carouselDesing: {
-    type: String,
-  },
+  // link: { type: String, required: false },
 
-  openCarousel: {
-    type: Boolean,
-  },
+  // long: { type: String, required: false },
+  // creator: { type: String, required: false },
+  // threeSixtyImage: { type: String, required: false },
+  // userId: { type: String, required: false },
+  // time: { type: String, required: false },
+  // passwordo: {
+  //   type: String,
+  // },
+  // havePassword: {
+  //   type: Boolean,
+  // },
+  // useit: {
+  //   type: Boolean,
+  // },
+  // publish: {
+  //   type: Boolean,
+  // },
+  // rotation: {
+  //   type: Boolean,
+  // },
+  // openDescription: {
+  //   type: Boolean,
+  // },
+  // loop: {
+  //   type: Boolean,
+  // },
+  // direction: {
+  //   type: String,
+  // },
+  // zoom: {
+  //   type: String,
+  // },
+  // zoom: {
+  //   type: String,
+  // },
+  // pause: {
+  //   type: Boolean,
+  // },
+  // showImageFeaturedInPause: {
+  //   type: Boolean,
+  // },
+  // disTourTitle: {
+  //   type: Boolean,
+  // },
+  // playicon: {
+  //   type: String,
+  // },
+  // pauseOpacity: {
+  //   type: String,
+  // },
+  // cssTourTitle: {
+  //   type: String,
+  // },
+  // EnableLine: {
+  //   type: Boolean,
+  // },
+  // LineTitle: {
+  //   type: String,
+  // },
+  // tourHd: {
+  //   type: String,
+  // },
+  // rotationSpeed: { type: String },
+
+  // carouselDesing: {
+  //   type: String,
+  // },
+
+  // openCarousel: {
+  //   type: Boolean,
+  // },
 });
 
 module.exports = mongoose.model('Place', placeSchema);
